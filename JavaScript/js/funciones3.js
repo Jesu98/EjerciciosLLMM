b40a80c9b33bd609e4ecf41016a8291ed0c03f2e
Jesu98 <92162454+Jesu98@users.noreply.github.com>
2022-02-09 14:17:13 +0100

    Primer commit

---
+++ JavaScript/js/funciones3.js
@@ -0,0 +1,11 @@
+function cambiarEstado()
+{
+	let nombre_clase=document.getElementById("parrafo_lorem").className;
+	if (nombre_clase=="rojo")
+		{document.getElementById("parrafo_lorem").className="verde";
+		document.getElementById("parrafo_lorem").innerHTML="Verde";}
+	else
+		{document.getElementById("parrafo_lorem").className="rojo";
+		document.getElementById("parrafo_lorem").innerHTML="Rojo";}
+	
+}
